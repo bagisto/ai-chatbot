c0c3ed4cd203c186c84ab59792520c078e26b89a
abhijit kumar <abhijit.kumar018@webkul.in>
2024-06-27 11:48:09 +0530

    fix : chatbot enabling issue

---
+++ src/_next/static/4ggbTbspWzyVy1iJzEBs_/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/901-728071fd042501ef.js","static/chunks/pages/index-dd7779cd75507c0c.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
